18fb92e7218ec2d7994c7b183a4ad5e86e2a8127
DanilPidhainyi <61890854+DanilPidhainyi@users.noreply.github.com>
2020-06-11 18:06:56 +0300

    Update Main.java

--- lab 7/lab7/Main.java
+++ lab 7/lab7/Main.java
@@ -6,14 +6,37 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+        // перший конструктор
         MySet newSet0 = new MySet();
+        // другий
         MySet newSet1 = new MySet(new FromSocket("Холодильник", 5, 100, true));
+        // третій
         HashSet<FromSocket> hashSet = new HashSet<>();
         FromSocket phone = new FromSocket("Телефон", 50, 10, true);
-        hashSet.add(new FromSocket("Холодильник", 5, 100, true));
+        FromSocket refrigerator = new FromSocket("Холодильник", 5, 100, true);
+        hashSet.add(refrigerator);
         hashSet.add(phone);
         MySet newSet2 = new MySet(hashSet);
-        newSet2.remove(phone);
+
+        // методи
+        System.out.println("Початковий: ");
+        newSet2.show();
+        HashSet<FromSocket> setTest = new HashSet<>();
+        setTest.add(phone);
+        setTest.add(new FromSocket("", 5, 20, false));
+        newSet2.removeAll(setTest);
+        System.out.println("Після видалення: ");
+        newSet2.show();
+        System.out.println("Зробимо retainAll");
+        newSet2.retainAll(hashSet);
+        newSet2.show();
+        System.out.println("Зробимо removeAll");
+        HashSet<FromSocket> refSet = new HashSet<>();
+        refSet.add(refrigerator);
+        //newSet2.removeAll(refSet);
+        newSet2.remove(refrigerator);
+        newSet2.show();
+        newSet2.toArray();
+
     }
 }
-
